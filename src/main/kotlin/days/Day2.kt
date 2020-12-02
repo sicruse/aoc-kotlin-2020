56c059c8d9786ee18b229cea775337e8d32b5cc1
Si Cruse <si@sicruse.com>
2020-12-02 12:29:33 -0500

    replace filter with count

--- src/main/kotlin/days/Day2.kt
+++ src/main/kotlin/days/Day2.kt
@@ -4,35 +4,34 @@ class Day2 : Day(2) {
 
     private val passwords: List<Password> by lazy { inputList.map { Password.deserialize(it) } }
 
-    class Password (
-            val parameter1: Int,
-            val parameter2: Int,
-            val element: Char,
-            val password: String
-            )
-    {
+    class Password(
+        val parameter1: Int,
+        val parameter2: Int,
+        val element: Char,
+        val password: String
+    ) {
         companion object {
             private val destructuredRegex = "([0-9]+)-([0-9]+)\\s([a-z]):\\s([a-z]+)".toRegex()
             fun deserialize(text: String): Password {
-                destructuredRegex.matchEntire (text)
+                destructuredRegex.matchEntire(text)
                     ?.destructured
                     ?.let { (_parameter1, _parameter2, _element, _password) ->
                         return Password(_parameter1.toInt(), _parameter2.toInt(), _element[0], _password)
                     }
-                    ?: throw IllegalArgumentException ("Bad input '$text'")
+                    ?: throw IllegalArgumentException("Bad input '$text'")
             }
         }
     }
 
     override fun partOne(): Any {
-        return passwords.filter { pw ->
+        return passwords.count { pw ->
             val occurrences = pw.password.count { it == pw.element }
             pw.parameter1 <= occurrences && occurrences <= pw.parameter2
-        }.count()
+        }
     }
 
     override fun partTwo(): Any {
-        return passwords.filter { pw ->
+        return passwords.count { pw ->
             val element1 = pw.password[pw.parameter1 - 1]
             val element2 = pw.password[pw.parameter2 - 1]
             when {
@@ -40,6 +39,6 @@ class Day2 : Day(2) {
                 (element1 == pw.element || element2 == pw.element) -> true
                 else -> false
             }
-        }.count()
+        }
     }
 }
