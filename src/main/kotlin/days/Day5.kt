1f48e98073b8b881464c9c0668a63a9fe20d1c44
Si Cruse <si@sicruse.com>
2020-12-05 15:42:13 -0500

    Day5 complete

--- src/main/kotlin/days/Day5.kt
+++ src/main/kotlin/days/Day5.kt
@@ -1,18 +1,54 @@
 package days
 
 import days.Day
+import kotlin.jvm.Throws
 
 class Day5 : Day(5) {
 
+    // Seat Codes look like this
+    // FBFBBFFRLR
+    // FBFBBFF    == 7 bit Row
+    //        RLR == 3 bit Column
+
+    private val rowBits = 7
+    private val colBits = 3
+
+    private val rowLookup = Lookup(rowBits, 'F', 'B')
+    private val colLookup = Lookup(colBits, 'L', 'R')
+
+    private val seatIDs by lazy {
+        inputList.map { seatCode ->
+            val row = rowLookup.search(seatCode.take(rowBits))
+            val col = colLookup.search(seatCode.drop(rowBits))
+            row * 8 + col
+        }.sorted()
+    }
+
+    class Lookup(
+        private val bits: Int,
+        private val low: Char,
+        private val high: Char
+    ) {
+        @Throws
+        fun search(code: String, lower: Int = 0, upper: Int = (1 shl bits) - 1): Int {
+            val mid = lower + (upper - lower + 1) / 2
+            return when {
+                code.length == 1 && code.first() == low -> lower
+                code.length == 1 -> upper
+                code.first() == low -> search(code.drop(1), lower, mid - 1)
+                code.first() == high -> search(code.drop(1), mid, upper)
+                else -> throw IllegalArgumentException("Its all broken")
+            }
+        }
+    }
+
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        return seatIDs.last()
     }
 
     override fun partTwo(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        val allSeatIDs = seatIDs.first()..seatIDs.last()
+        return (allSeatIDs - seatIDs).first()
     }
 }
+
--- src/test/kotlin/days/Day5Test.kt
+++ src/test/kotlin/days/Day5Test.kt
@@ -7,14 +7,47 @@ import org.junit.Test
 class Day5Test {
 
     private val day = Day5()
+    private val rowSearch = Day5.Lookup(7,'F', 'B')
+
+    @Test
+    fun testLastRowSearch() {
+        assertThat(rowSearch.search("F", 44,45), `is`(44))
+    }
+
+    @Test
+    fun test2ndRowSearch() {
+        assertThat(rowSearch.search("FF", 44,47), `is`(44))
+    }
+
+    @Test
+    fun test3rdRowSearch() {
+        assertThat(rowSearch.search("BFF", 40,47), `is`(44))
+    }
+
+    @Test
+    fun test4thRowSearch() {
+        assertThat(rowSearch.search("BBFF", 32,47), `is`(44))
+    }
+
+    @Test
+    fun testRowSearch() {
+        assertThat(rowSearch.search("FBFBBFF"), `is`(44))
+    }
+
+    private val seatSearch = Day5.Lookup(3, 'L', 'R')
+
+    @Test
+    fun testSeatSearch() {
+        assertThat(seatSearch.search("RLR", 0, 7), `is`(5))
+    }
 
     @Test
     fun testPartOne() {
-        assertThat(day.partOne(), `is`("Whatever is expected"))
+        assertThat(day.partOne(), `is`(357))
     }
 
-    @Test
-    fun testPartTwo() {
-        assertThat(day.partTwo(), `is`("Whatever is expected"))
-    }
+//    @Test
+//    fun testPartTwo() {
+//        assertThat(day.partTwo(), `is`("Whatever is expected"))
+//    }
 }
