f23623102cc8c7e9b3dcbf3fb4fe239f71a0ea89
Si Cruse <si@sicruse.com>
2020-12-06 07:50:01 -0500

    Day6 complete

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -4,15 +4,34 @@ import days.Day
 
 class Day6 : Day(6) {
 
+    private val groups: List<List<String>> by lazy {
+        inputString
+            // split the input at blank lines to form record groups
+            .split("\\n\\n".toRegex())
+            .map { group ->
+                // for every record text block gather the individual responses by splitting at newlines
+                group.split("\\n".toRegex())
+            }
+    }
+
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        // return the sum "yes" answers by group
+        return groups
+                // join group of responses to form unique set of answers
+            .map { groupOfResponses -> groupOfResponses.joinToString(separator = "").toSet() }
+                // sum the unique answers per group
+            .fold(0) { acc, set -> acc + set.size }
     }
 
     override fun partTwo(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        // return the sum of questions which *everyone* in a group answered yes
+        return groups
+                // for each group of responses convert each participants response to a set of unique answers
+            .map { groupOfResponses -> groupOfResponses.map { response -> response.toSet() } }
+                // for each set of responses in each group determine the set of questions which every participant responded yes
+            .map { groupOfResponses -> groupOfResponses.fold(groupOfResponses.first()) { acc, set -> acc.intersect(set) } }
+                // sum the sets of answers which *everyone* in the group answered yes
+            .fold(0) { acc, set -> acc + set.size }
     }
+
 }
--- src/test/kotlin/days/Day6Test.kt
+++ src/test/kotlin/days/Day6Test.kt
@@ -10,11 +10,11 @@ class Day6Test {
 
     @Test
     fun testPartOne() {
-        assertThat(day.partOne(), `is`("Whatever is expected"))
+        assertThat(day.partOne(), `is`(11))
     }
 
     @Test
     fun testPartTwo() {
-        assertThat(day.partTwo(), `is`("Whatever is expected"))
+        assertThat(day.partTwo(), `is`(6))
     }
 }
