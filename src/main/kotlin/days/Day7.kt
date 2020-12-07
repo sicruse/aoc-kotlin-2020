c2e40bbc03801ad2eb360a713f32caaa273f59c9
Si Cruse <si@sicruse.com>
2020-12-07 18:50:50 -0500

    Day7 complete

--- src/main/kotlin/days/Day7.kt
+++ src/main/kotlin/days/Day7.kt
@@ -1,18 +1,59 @@
 package days
 
-import days.Day
-
 class Day7 : Day(7) {
 
+    private val targetBag = "shiny gold"
+
+    private val bags: Map<String, List<Rule>> by lazy {
+        inputList
+            .map { description -> bagFromDescription(description) }
+            .associateBy({ it.first }, { it.second })
+    }
+
+    data class Rule(val name: String, val quantity: Int)
+
+    fun bagFromDescription(description: String) : Pair<String, List<Rule>> {
+        val nameAndContents = description.split(" bags contain ")
+        val name = nameAndContents.first()
+        val contents = nameAndContents.last()
+        return if (contents == "no other bags.") Pair(name, emptyList())
+        else {
+            val bags =
+                contents.split(", ")
+                    .map{ bag ->
+                        val bagName = bag.drop(2).split(" bag").first()
+                        val quantity = bag.first().toString().toInt()
+                        Rule(bagName, quantity)
+                    }
+            return Pair(name, bags)
+        }
+    }
+
+    fun findBagsWhichContain(name: String): List<Pair<String, List<Rule>>> {
+        val bagsWhichDirectlyContain = bags.filter { bag -> bag.value.any{ content -> content.name == name } }.toList()
+        val bagsWhichIndirectlyContain = bagsWhichDirectlyContain.flatMap { bag -> findBagsWhichContain(bag.first) }
+        return (bagsWhichDirectlyContain + bagsWhichIndirectlyContain).distinct()
+    }
+
+    fun findBagsRequired(name: String, num: Int): List<Pair<String,Int>> {
+        val target = bags[name]
+        val additionalBags = target?.flatMap { containedBag ->
+            findBagsRequired(containedBag.name, containedBag.quantity * num)
+        }
+        return when {
+            additionalBags == null -> listOf(Pair(name, num))
+            else -> listOf(Pair(name, num)) + additionalBags
+        }
+    }
+
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        val b = findBagsWhichContain(targetBag)
+        return b.count()
     }
 
     override fun partTwo(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        val b = findBagsRequired(targetBag, 1)
+        return b.fold(0) { acc, pair -> acc + pair.second } - 1
     }
 }
+
--- src/test/kotlin/days/Day7Test.kt
+++ src/test/kotlin/days/Day7Test.kt
@@ -10,11 +10,11 @@ class Day7Test {
 
     @Test
     fun testPartOne() {
-        assertThat(day.partOne(), `is`("Whatever is expected"))
+        assertThat(day.partOne(), `is`(4))
     }
 
     @Test
     fun testPartTwo() {
-        assertThat(day.partTwo(), `is`("Whatever is expected"))
+        assertThat(day.partTwo(), `is`(32))
     }
 }
