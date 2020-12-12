07ad738d7f04252f29502cbda3d2e9cbbfd4d6e6
Si Cruse <si@sicruse.com>
2020-12-12 16:38:57 -0500

    prep for next few days

---
+++ src/test/kotlin/days/Day20Test.kt
@@ -0,0 +1,20 @@
+package days
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Test
+
+class Day20Test {
+
+    private val day = Day20()
+
+    @Test
+    fun testPartOne() {
+        assertThat(day.partOne(), `is`(0 ))
+    }
+
+//    @Test
+//    fun testPartTwo() {
+//        assertThat(day.partTwo(), `is`(0 ))
+//    }
+}
