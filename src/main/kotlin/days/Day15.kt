9d5a5211febe10923d039908a446416a2f437b49
Si Cruse <si@sicruse.com>
2020-12-15 09:56:09 -0500

    day15 Done - note memory exhaustion in test case 2, however works for real input!

--- src/main/kotlin/days/Day15.kt
+++ src/main/kotlin/days/Day15.kt
@@ -4,15 +4,49 @@ import days.Day
 
 class Day15 : Day(15) {
 
+    private val numbers: List<Int> by lazy {
+        inputString.split(",").map{ it.toInt() }
+    }
+
+    val spoken: Sequence<Int> = sequence {
+        val history = mutableMapOf<Int, MutableList<Int>>()
+        var turn = 1
+        var current:Int? = null
+        while(true) {
+            if (current == null || turn <= numbers.size) {
+                // We are in the preamble of the game, just read from our input list
+                current = numbers[turn - 1]
+            } else {
+                // Take the last value of current and deduce the next current
+                val lastTime = history[current]!!
+                if (lastTime.size == 1) {
+                    current = 0
+                } else {
+                    val plays = lastTime.takeLast(2)
+                    current = plays.last() - plays.first()
+                }
+            }
+
+            // Update the history
+            if (history[current] == null)
+                history[current] = mutableListOf(turn)
+            else
+                history[current]!!.add(turn)
+
+            yield(current)
+            turn++
+        }
+    }
+
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        val result = spoken.elementAt(2020 - 1)
+
+        return result
     }
 
     override fun partTwo(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        val result = spoken.elementAt(30000000 - 1)
+
+        return result
     }
 }
